ac9d97c3512191c5e6bea2d5eef26965df39cc8f
BadriKumar2000 <g.badrikumar2000@gmail.com>
2024-03-13 09:37:20 +0530

    commiting the main files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import CommentsComponent from "./components/CommentsComponent/index.js";
+//import Clock from "./components/Clock/index.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import logo from "./logo.svg";
+import "./App.css";
+import Greeting from "./components/Greeting/index.js";
+import BmiCalculator from "./components/BmiCalculator/index.js";
+
+// class App extends Component {
+//   state = {
+//     showClock: false,
+//   };
+
+//   showClock = () => {
+//     const { showClock } = this.state;
+//     this.setState({ showClock: !showClock });
+//   };
+
+//   hideClock = () => {
+//     const { showClock } = this.state;
+//     this.setState({ showClock: !showClock });
+//   };
+
+//   render() {
+//     const { showClock } = this.state;
+//     return (
+//       <div className="main-container d-flex justify-content-center flex-column align-items-center bg-dark text-center">
+//         <div className="row">
+//           <div className="col-12">
+//             <h1>Clock</h1>
+//             {showClock && <Clock />}
+//             <button className="btn btn-primary mx-2" onClick={this.showClock}>
+//               Show
+//             </button>
+//             <button className="btn btn-primary mx-2" onClick={this.hideClock}>
+//               Hide
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+class App extends Component {
+  render() {
+    return <BmiCalculator />;
+  }
 }
 
 export default App;
